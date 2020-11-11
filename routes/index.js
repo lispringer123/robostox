84d956f3b41e411aad3734448160c4f1663d50e5
Liam Springer <lispringer@davidson.edu>
2020-11-10 20:01:33 -0500

    more logging on heroku

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/stock_buy', function(req, res, next){
-   console.log('!!! incoming', req.body)
-   res.status(200)
-   res.end()
-})
+
 
 
 module.exports = router;
